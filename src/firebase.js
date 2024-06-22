38ba0a1d7a64193a6bc0b2eff89cbe4e32b31fc7
SantiagoFeli <sfeliciani@correo.unimet.edu.ve>
2024-06-22 15:01:45 -0400

    firebase al dia

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDskA3nGbopZXE10o5ogpdYyTk9MwL6mII",
+  authDomain: "csccoffeebar.firebaseapp.com",
+  projectId: "csccoffeebar",
+  storageBucket: "csccoffeebar.appspot.com",
+  messagingSenderId: "95927833665",
+  appId: "1:95927833665:web:0a108f62aaa34edd4f62c0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
