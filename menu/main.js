a8c32fe32ba4d0bab815a44ab6884495a2e2dc19
Nicolepinto09 <nicole.pinto@correo.unimet.edu.ve>
2024-06-24 18:04:31 -0400

    Ventanas de acceso

---
+++ menu/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function()) {
+    var loginBtn = document.querySelector('.iniciar button');
+    var loginModal = document.getElementById('loginModal');
+    var closeLogin = document.getElementById('closeLogin');
+
+
+    var registerBtn = document.querySelector('.registrate button');
+    var registerModal = document.getElementById('registerModal');
+    var closeRegister = document.getElementById('closeRegister');
+}
