a08cb8f1b9181d6ee3c101c5cd6a0ec1f5235cda
Nicolepinto09 <nicole.pinto@correo.unimet.edu.ve>
2024-06-24 22:54:09 -0400

    arreglos login

--- src/componentes/LoginModal.jsx
+++ src/componentes/LoginModal.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { auth, googleProvider, facebookProvider } from './firebaseConfig';
+import { signInWithPopup } from 'firebase/auth';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import './LoginModal.css';
 
-function LoginModal({ closeModal }) {
+function LoginModal({closeModal}) {
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      alert("Ha iniciado sesión con Google correctamente");
+    } catch (error) {
+      console.error("Error en inicio de sesión con Google: ", error);
+    }
+  };
+
+  const signInWithFacebook = async () => {
+    try {
+      await signInWithPopup(auth, facebookProvider);
+      alert("Ha iniciado sesión con Facebook correctamente");
+    } catch (error) {
+      console.error("Error en el inicio de sesión con Facebook: ", error);
+    }
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
@@ -13,6 +35,13 @@ function LoginModal({ closeModal }) {
           <input type="password" id="loginPassword" name="loginPassword" required />
           <button type="submit">Iniciar Sesión</button>
         </form>
+        <hr />
+        <button className="social-button facebook-button" onClick={signInWithFacebook}>
+          <FaFacebook /> INICIAR CON FACEBOOK
+        </button>
+        <button className="social-button google-button" onClick={signInWithGoogle}>
+          <FaGoogle /> INICIAR CON GOOGLE
+        </button>
       </div>
     </div>
   );
